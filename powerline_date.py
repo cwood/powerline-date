ec5f7ab656e51ca8eda8be4c0d58555d18d72a79
Colin Wood <cwood06@gmail.com>
2019-11-20 20:53:39 +0000

    Allow highlight group to prefix with TZ and remove uneeded arg

--- powerline_date.py
+++ powerline_date.py
@@ -4,14 +4,17 @@ from datetime import datetime
 from dateutil import tz
 
 
-def date(pl, format='%Y-%m-%d', istime=False, timezone=None):
+def date(pl, format='%Y-%m-%d', timezone=None):
+    group = ['time']
     if not timezone:
         now = datetime.now()
     else:
         now = datetime.now(tz.gettz(timezone))
+        group.insert(0, "time:%s" % timezone)
+
 
     return [{
-        'contents': now.strftime(format),
-        'highlight_groups': (['time'] if istime else []) + ['date'],
-        'divider_highlight_group': 'time:divider' if istime else None,
+        'contents': now.strftime(format).strip(),
+        'highlight_groups': group,
+        'divider_highlight_group': 'time:divider',
     }]
